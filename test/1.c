0ac592fa29e8f72b6b3d2f573b744dc1c6df2829
hit-fushibo <fushibo123123123@163.com>
2023-05-08 10:32:02 +0800

    test

--- test/1.c
+++ test/1.c
@@ -1,29 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main (int argc,char *argv[])
-{
-    long long int a,b,c;
-    //for(a=0;a<3218;a++)
-    //{
-    //    for(b=0;b<3128;b++)
-    //    {
-    //        for(c=0;c<3218;c++)
-    //        {
-    //            if(a*b*c==3218)
-    //            {
-    //                printf("%d %d %d\n",a,b,c);
-    //            }
-    //        }
-    //    }
-    //}
-    for(a=0;a<1609;a++)
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+    int i=0,flag=1,j=1;
+    int * returndig = 0;
+    for(i=0;i<digitsSize;i++)
     {
-        if(1609%a==0)
+        if(digits[i]!=9)
         {
-            printf("%d\n",a);
+            flag=0;
+            break;
         }
     }
-    
+    //printf("%d %d\n",flag,digitsSize);
+    *returnSize=digitsSize+flag;
+    returndig = (int *)malloc(sizeof(int)*(digitsSize+flag));
+    if(flag==0)
+    {
+        for(int i = 0 ;i<digitsSize;i++)
+        {
+            returndig[i] = digits[i];
+        }
+        returndig[digitsSize-1]+=1;
+        for(int i=digitsSize-1;i>0;i--)
+        {
+            printf("%d ",i);
+            if(returndig[i]>=10)
+            {
+                returndig[i]%=10;
+                returndig[i-1]+=1;
+            }
+        }
+    }
+    else
+    {
+        returndig[0]=1;
+        for(int i=1;i<(digitsSize+flag);i++)
+            returndig[i]=0;
+    }
+    return returndig;
+}
+
+int main (void)
+{
+    int test[]={1,2};
+    int len=sizeof(test)/sizeof(int);
+    int relen=0;
+    int * p =plusOne(test,len,&relen);
+    for (int i=0;i<relen;i++)
+    {
+        printf("%d ",p[i]);
+    }
+
     return 0;
+    
 }
