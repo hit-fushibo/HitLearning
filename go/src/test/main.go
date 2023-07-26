90ecdbb98a9eb0cfae0c4c2eb0049f19dc4fcdad
hit-fushibo <fushibo123123123@163.com>
2023-07-26 11:41:56 +0800

    change

--- go/src/test/main.go
+++ go/src/test/main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
 )
 
+func helloHandleFunc(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./test.tmpl")
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	name := "i love go"
+	t.Execute(w, name)
+}
+
 func main() {
-	fmt.Println("hello word")
+	http.HandleFunc("/", helloHandleFunc)
+	http.ListenAndServe(":8086", nil)
 }
