3be56bc587b2bb5fdbb5d11eaed1aa18c1bc83c6
hit-fushibo <fushibo123123123@163.com>
2023-07-26 11:42:09 +0800

    change

--- test/1.c
+++ test/1.c
@@ -1,64 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int aa(int x,int y);
-int recod[5][5]={0};
-
 int main (void)
 {
-    printf("%d",aa(4,3));
+    char a[10];
     
+    gets(a);
+    for(int i =0;i<10;i++)
+    {
+        if(a[i]=='\0')
+        break;
+        a[i]='a'+(a[i]-'a')%8;
+    }
+    printf("%s",a);
     return 0;
-}
-
-int aa(int x,int y)
-{
-    if(x==0&&y>=0)
-    {
-        recod[x][y]=1;
-        return 1;
-    }
-    else if (x==1&&y==0)
-    {
-        recod[x][y]=2;
-        return 2;
-    }
-    else if (x>=2&&y==0)
-    {
-        recod[x][y]=x+2;
-        return x+2;
-    }
-    else
-    {
-        if(recod[x-1][y]==0)
-        {
-            recod[x-1][y]==aa(x-1,y);
-            int tmp=recod[x-1][y];
-            if(recod[tmp][y-1]==0)
-            {
-                recod[tmp][y-1]=aa(recod[x-1][y],y-1);
-                return recod[tmp][y-1];
-            }
-            else
-            {
-                return recod[tmp][y-1];
-            }
-        }
-        else
-        {
-            int tmp=recod[x-1][y];
-            if(recod[tmp][y-1]==0)
-            {
-                recod[tmp][y-1]=aa(recod[x-1][y],y-1);
-                return recod[tmp][y-1];
-            }
-            else
-            {
-                return recod[tmp][y-1];
-            }
-
-        }
-    }
-    
-    
 }
