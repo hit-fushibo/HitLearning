c7a2e19a1106912dc080e22f6a36fca3c19db370
hit-fushibo <fushibo123123123@163.com>
2023-10-19 22:51:55 +0800

    文件结构更改

--- Learning/Computational_Modeling/Project/main.py
+++ Learning/Computational_Modeling/Project/main.py
@@ -23,28 +23,6 @@ def func(X,t,rho,phi,beta,epsilon,omega,alpha,theta,gamma_I,gamma_q,d_I,d_q):
             b-alpha*X[-2],\
             alpha*X[-2]-gamma_q*X[-1]-d_q*X[-1]+theta*X[2]]
 
-
-# #bad
-# T=1
-# rho=0.2
-# beta=9.08*10**-4
-# theta=0.2
-# gamma_q=0.2
-# d_q=0.7
-
-
-
-#average
-T=7
-rho=0.5
-beta=5
-theta=0.5
-gamma_q=0.5
-d_q=0.5
-
-
-
-
 def D_count(T,rho,beta,theta,gamma_q,d_q):
     X0=[10**5,109,12,0,0,0,0,21]
     
@@ -53,11 +31,12 @@ def D_count(T,rho,beta,theta,gamma_q,d_q):
     D=s1[-1,4]
     return D
 
+
 #average
 T=7
 rho=0.5 #0.1-0.9
 beta=5 #0.5-10
-theta=0.5 #0.1-0.9
+theta=0.9 #0.1-0.9
 gamma_q=0.5 #0.1-0.9
 d_q=1-gamma_q
 
